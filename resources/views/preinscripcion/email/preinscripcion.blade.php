15532c83b703f47924994f9debdd98406361a9fa
Jose Luis <joseluis901@hotmail.com>
2017-08-27 17:08:39 -0500

    Se agregan cambios para:
    - al preinscribir, enviar el mail para que el usuario realice el cargue de documentos
    
    - Al oprimir el botón de validación comercial enviar el correo para la entrevista.

--- resources/views/preinscripcion/email/preinscripcion.blade.php
+++ resources/views/preinscripcion/email/preinscripcion.blade.php
@@ -1,6 +1,11 @@
 Hola {{$persona->nombres }},
 
 Agradecemos que hayas confiado en nosotros para crear tu futuro profesional. Te confirmamos que has quedado preinscrito(a) al programa {{$inscripcion->nombre_programa}}.
+
+Para continuar tu proceso te pedimos que nos proporciones algunos documentos, en este link podrás encontras mas detalles:
+
+<a href="{{ $link = url('prepararCargaDocumentos').'/'.urlencode($proceso->id_proceso_admon) }}"> {{ $link }} </a>
+
 Pronto nos comunicaremos contigo para continuar con tu proceso!
 
 
