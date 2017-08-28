fc811e289f9d5ef4bd16afcd145a6687f59e2242
Jose Luis <joseluis901@hotmail.com>
2017-08-27 21:00:52 -0500

    Se modifica el correo de la entrevista para cifrar los datos de el id del proceso

--- resources/views/entrevista/email/cuestionarioEntrevista.blade.php
+++ resources/views/entrevista/email/cuestionarioEntrevista.blade.php
@@ -1,3 +1,5 @@
 Hola {{$persona->nombres }}, Continúa tu proceso de admisión en la Ibero!!
-
-Queremos saber más acerca de Tí, Por favor completa un breve cuestionario: <a href="{{ $link = url('entrevista').'/'.urlencode($procesoAdmon->id_proceso_admon) }}"> {{ $link }} </a>
+<br><br>
+Queremos saber más acerca de Tí, Por favor completa un breve cuestionario: 
+<br><br>
+<a href="{{ $link = url('entrevista').'/'.urlencode(encrypt($procesoAdmon->id_proceso_admon)) }}"> {{ $link }} </a>
