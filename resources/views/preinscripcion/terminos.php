ecf34ba5a70fc61a42ff7b56695531d176b8e9bb
Camilo Hurtado <ingcamhurtado@gmail.com>
2017-11-16 19:07:59 -0500

    Se realizan cambios de terminos y condiciones

--- resources/views/preinscripcion/terminos.php
+++ resources/views/preinscripcion/terminos.php
@@ -1,3 +1,10 @@
+ï»¿
+<script type="text/javascript" language="javascript">
+function onClick() {
+   window.close();
+}
+</script>
+
 
 <div class="container">
     <div class="row">
@@ -5,8 +12,12 @@
             <div class="panel panel-default">
                 <div class="form-group">
 					<label for="task-name" class="col-sm-3 control-label">
-					Al diligenciar esta solicitud de admisión autorizo a la CORPORACIÓN UNIVERSITARIA IBEROAMERICANA para el uso de la información aquí indicada, así como para que me envíen información relevante, en ejercicio de sus fines como Institución de Educación Superior, la cual será depositada en sus Bases de Datos, teniendo un manejo adecuado de la misma en cumplimiento de lo dispuesto en el Decreto 1377 de 2013 (Ley 1581 de 2012 - Protección de Datos Personales)!
+					Al diligenciar esta solicitud de admisiÃ³n autorizo a la CORPORACIÃ“N UNIVERSITARIA IBEROAMERICANA para el uso de la informaciÃ³n aquÃ­ indicada, asÃ­ como para que me envÃ­en informaciÃ³n relevante, en ejercicio de sus fines como InstituciÃ³n de EducaciÃ³n Superior, la cual serÃ¡ depositada en sus Bases de Datos, teniendo un manejo adecuado de la misma en cumplimiento de lo dispuesto en el Decreto 1377 de 2013 (Ley 1581 de 2012 - ProtecciÃ³n de Datos Personales)
 					</label>
+				</div>
+				
+				<div class="form-group">
+					<a href="javascript:onClick()" class="panel-heading">Entendido</a> 
 				</div>
 
               
